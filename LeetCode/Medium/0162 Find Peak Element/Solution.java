1a52712f292f487f37851b6d6d46f38f4626b0ce
Saumya Sinha <saumyasinha0706@gmail.com>
2026-07-13 23:17:38 +0530

    Add solution for LeetCode - Find Peak Element

--- LeetCode/Medium/0162 Find Peak Element/Solution.java
+++ LeetCode/Medium/0162 Find Peak Element/Solution.java
@@ -2,9 +2,14 @@ class Solution {
     public int findPeakElement(int[] nums) {
         if (nums.length == 1)
         return 0;
-        for(int i = 1; i<nums.length -1; i++){
-            if(nums[i]>nums[i-1]&&nums[i]>nums[i+1])
-            return i;
+        for (int i = 0; i < nums.length; i++) {
+            // Check left neighbor if exists
+            boolean left = (i == 0) || (nums[i] >= nums[i - 1]);
+            // Check right neighbor if exists
+            boolean right = (i == nums.length - 1) || (nums[i] >= nums[i + 1]);
+
+            // If both conditions are true
+            if (left && right) return i;
         }
         return -1;
     }
